662e422037c8178789d637ac821fc51e8d2d25b7
gulkaa <gulshatidiyatova495@gmail.com>
2024-09-04 17:17:22 +0300

    Практическая №1

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// 'use strict';
+// let age = prompt('Сколько вам лет?', 'Неизвестно');
+// //alert(`Вам ${age} лет`);
+// let isTrue = confirm(`Вам действительно ${age} лет?`);
+// alert (isTrue);
+
+//ЗАДАНИЕ 1
+// alert ('Я JavaScript');
+
+//ЗАДАНИЕ 2
+'use strict';
+let name = prompt('Ваше имя?', 'Неизвестно');
+let surname = prompt('Ваша фамилия?', 'Неизвестно');
+let otche = prompt('Ваше oтчество?', 'Неизвестно');
+alert (`Вы ${name} ${surname} ${otche}?`);
+let isTrue = confirm(`Вы действительно ${name} ${surname} ${otche}?`);
+alert (isTrue);
